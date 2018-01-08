85cb05059d25024018fee62c29233eeec605162d
nascimbr <bruno.nascimento@schindler.com>
2018-01-08 12:15:01 -0200

    Solution to Amazon Test

--- src/br/com/algorithm/AmazonTest2.java
+++ src/br/com/algorithm/AmazonTest2.java
@@ -1,69 +1,58 @@
 package br.com.algorithm;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class AmazonTest2 {
 
 	public static void main(String[] args) {
-		int capacidadeTotal = 100;
-		
-		int items = 2;
-				
-		int[] peso = {70, 10, 20};
+		int maxMotorcycleWeight = 100;
+		int[] packagesWeight = {70, 10, 20, 50,100,1};
 
-		int min = minimumNumberOfTrips(capacidadeTotal, peso);
-		
-		System.out.println();
+		int min = minimumNumberOfTrips(maxMotorcycleWeight, packagesWeight);
+
 		System.out.println(min);
-		
-//		int[][] solucao = new int[items+1][capacidadeTotal+1];
-//
-//		for(int line = 0 ; line < items; line++){
-//			solucao[line][0] = 0;
-//		}
-//		
-//		for(int col = 0 ; col < capacidadeTotal; col++){
-//			solucao[0][col] = 0;
-//		}
-//		
-//		for(int item = 1 ; item <= items; item++){
-//
-//			for(int capacidade = 1; capacidade <= capacidadeTotal; capacidade++){
-//
-//				if(peso[item-1] > capacidade){
-//					solucao[item][capacidade] = solucao[item-1][capacidade];
-//				}
-//				else {
-////					V[item][weight]=Math.max (val[item-1]+V[item-1][weight-wt[item-1]], V[item-1][weight]);					
-//					solucao[item][capacidade] = Math.max(valor[item-1]+solucao[item-1][capacidade-peso[item-1]], solucao[item-1][capacidade]);
-//					
-//				}
-//			}
-//		}
-
-//		printSolution(solucao);
-//		
-//		System.out.println(solucao[items][capacidadeTotal]);
 	}
 
 	public static int minimumNumberOfTrips(int tripMaxWeight, int[] packagesWeight) {
-	
-		Arrays.sort(packagesWeight);
-		
-		
-		
-		return 0;
-	}
-	
-//	private static void printSolution(int[][] solucao) {
-//		  for (int[] rows : solucao) {
-//	            for (int col : rows) {
-//	                System.out.format("%5d", col);
-//	            }
-//	            System.out.println();
-//	        }
-//	}
+		int maxPackagePerTrip = 2;
+		int trips = 0;
 
+		int[] totalWeights = new int[packagesWeight.length];
+		int[] totalPacks = new int[packagesWeight.length];
+
+		Arrays.fill(totalPacks, 0);
+
+		for(int i=0; i < packagesWeight.length; i++){
+
+			if(i == 0){
+				totalPacks[i] = totalPacks[i]+1;
+				totalWeights[i] = packagesWeight[i];
+			}
+			else if(totalPacks[i-1] == maxPackagePerTrip) {
+				totalPacks[i] = 1;
+				totalWeights[i] = packagesWeight[i];
+			}
+			else if(packagesWeight[i]+totalWeights[i-1] > tripMaxWeight) {
+				totalPacks[i-1] = totalPacks[i-1]+1;
+				totalPacks[i] = 1;
+				totalWeights[i] = packagesWeight[i];
+			}
+			else {
+				totalPacks[i] = totalPacks[i-1]+1;
+				totalWeights[i] = packagesWeight[i]+totalWeights[i-1];
+			}
+		}
+
+		if(totalPacks[totalPacks.length-1] == 1){
+			totalPacks[totalPacks.length-1] += 1;
+		}
+
+		for(int pack : totalPacks){
+			if(pack == 2){
+				trips++;
+			}
+		}
+
+		return trips;
+	}
 }
